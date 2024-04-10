5c67ac103e5cdcc63bcfb0bad0665bbc9110d70a
Du Duong <khaidu2002@gmail.com>
2024-04-10 17:57:02 -0500

    dropdown and clean up scale

--- indoor_nav/Assets/Scripts/SetNavigationTarget.cs
+++ indoor_nav/Assets/Scripts/SetNavigationTarget.cs
@@ -2,33 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 public class SetNavigationTarget : MonoBehaviour
 {
     [SerializeField]
-    private Camera topDownCamera;
+    private TMP_Dropdown navTargetDropdown;
+
     [SerializeField]
-    private GameObject navTarget;
+    private List<Target> navTargets = new List<Target>();
 
     private NavMeshPath path; // The path to the navigation target
     private LineRenderer lineRenderer; // The line renderer to draw the path
 
+    private Vector3 targetPosition = Vector3.zero; // The position of the navigation target
+
     private bool lineToggle = false; // Toggle to show/hide the path
 
-    private void Start() {
+    private void Start()
+    {
         path = new NavMeshPath();
         lineRenderer = transform.GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        if (navTargets.Count > 0)
+        {
+            targetPosition = navTargets[0].PositionObject.transform.position;
+        }
     }
 
-    private void Update() {
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) {
-            lineToggle = !lineToggle;
-        }
-        if (lineToggle) {
-            NavMesh.CalculatePath(transform.position, navTarget.transform.position, NavMesh.AllAreas, path);
+    private void Update()
+    {
+        if (lineToggle && targetPosition != Vector3.zero)
+        {
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
             lineRenderer.positionCount = path.corners.Length;
             lineRenderer.SetPositions(path.corners);
-            lineRenderer.enabled = true;
         }
+    }
+
+    public void SetTarget(int index)
+    {
+        targetPosition = Vector3.zero;
+        string targetName = navTargets[index].Name;
+        Target cur = navTargets.Find(t => t.Name == (targetName));
+        if (cur != null)
+        {
+            targetPosition = cur.PositionObject.transform.position;
+        }
+    }
+
+    public void ToggleLine()
+    {
+        lineToggle = !lineToggle;
+        lineRenderer.enabled = lineToggle;
     }
 }
